3c6726dc337e87b1b7dd6b4292aa958ebf083773
Maxence <maxence.ho@i-bp.fr>
2020-10-23 15:03:24 +0200

    Initial Commit

---
+++ unit_test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  unit_test
+//
+//  Created by Maxence HO on 23/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ unit_test/unit_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  unit_testApp.swift
+//  unit_test
+//
+//  Created by Maxence HO on 23/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct unit_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
